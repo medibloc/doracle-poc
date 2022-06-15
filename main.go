4917128c56f252186ea2d5dfbf3a9ad3f59391e5
Youngjoon Lee <yjlee@medibloc.org>
2022-06-15 17:11:26 +0900

    fix

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const keyFilePath = "/data/oracle-key.sealed"
+
 func main() {
 	pInit := flag.Bool("init", false, "run doracle with the init mode")
 	pPeer := flag.String("peer", "", "a peer addr (do not use with -init)")
@@ -25,13 +27,22 @@ func main() {
 	}
 
 	if *pInit {
-		if err := runInit(); err != nil {
+		if err := generateAndSaveOracleKey(); err != nil {
 			log.Fatalf("failed to init: %v", err)
 		}
 	}
 
+	oraclePrivKey, err := loadOraclePrivKey()
+	if err != nil {
+		log.Fatalf("failed to load oracle key: %v", err)
+	}
+
+	myRouter := &MyRouter{
+		oraclePrivKey: oraclePrivKey,
+	}
+
 	router := mux.NewRouter()
-	router.HandleFunc("/join", handleJoin).Methods("POST")
+	router.HandleFunc("/join", myRouter.HandleJoin).Methods("POST")
 
 	srv := &http.Server{
 		Handler:      router,
@@ -44,7 +55,7 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
-func runInit() error {
+func generateAndSaveOracleKey() error {
 	oraclePrivKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return fmt.Errorf("failed to generate oracle key: %w", err)
@@ -56,15 +67,37 @@ func runInit() error {
 		return fmt.Errorf("failed to seal oracle key: %w", err)
 	}
 
-	path := "/data/oracle-key.sealed"
-	if err := ioutil.WriteFile(path, sealed, 0644); err != nil {
-		return fmt.Errorf("failed to write %s: %w", path, err)
+	if err := ioutil.WriteFile(keyFilePath, sealed, 0644); err != nil {
+		return fmt.Errorf("failed to write %s: %w", keyFilePath, err)
 	}
-	log.Printf("%s is written successfully", path)
+	log.Printf("%s is written successfully", keyFilePath)
 
 	return nil
 }
 
-func handleJoin(w http.ResponseWriter, r *http.Request) {
-	log.Println("join")
+func loadOraclePrivKey() (*rsa.PrivateKey, error) {
+	sealed, err := ioutil.ReadFile(keyFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %s: %w", keyFilePath, err)
+	}
+
+	unsealed, err := ecrypto.Unseal(sealed, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unseal oracle key: %w", err)
+	}
+
+	privKey, err := x509.ParsePKCS1PrivateKey(unsealed)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse oracle key: %w", err)
+	}
+
+	return privKey, nil
+}
+
+type MyRouter struct {
+	oraclePrivKey *rsa.PrivateKey
+}
+
+func (mr *MyRouter) HandleJoin(w http.ResponseWriter, r *http.Request) {
+	log.Printf("priv key: %v", mr.oraclePrivKey)
 }
